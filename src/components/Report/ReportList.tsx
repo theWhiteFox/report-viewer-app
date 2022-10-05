3b52dbc7ac07efa0a93ea045effdf78b63e1f498
Stíofán ♔ Ó Conchubhair <stevo.joc@gmail.com>
2022-10-05 16:17:48 +0100

    Update ReportList.tsx

--- src/components/Report/ReportList.tsx
+++ src/components/Report/ReportList.tsx
@@ -3,7 +3,7 @@
 // 3. report list item name description button open report result table
 // 4. billing period sort by date : 2 date pickers deafult date reange of 4 file jan 2017 dec 2017
 // /api/reports.json contains a list of available reports.
-import React, { useState } from 'react'
+import { cloneElement, ReactElement, useState } from 'react'
 import { styled } from '@mui/material/styles';
 import Box from '@mui/material/Box';
 import List from '@mui/material/List';
@@ -25,15 +25,20 @@ import DateTimePickers from './BasicDateRangePicker'
 import { ListItemText } from '@mui/material';
 
 const reports = {
+    reportId: '41',
+    billingPeriod: '201708'
+}
+
+const reportsMob = {
     reportId: '245',
     billingPeriod: '201708'
 }
 
-const numberOfReports = [1, 2, 3, 4]
+const numberOfReports = [1]
 
-function generate(element: React.ReactElement) {
+function generate(element: ReactElement) {
     return numberOfReports.map((value: any) =>
-        React.cloneElement(element, {
+        cloneElement(element, {
             key: value,
         }),
     );
@@ -44,16 +49,12 @@ const Demo = styled('div')(({ theme }) => ({
 }));
 
 const ReportList = () => {
-    const [secondary, setSecondary] = React.useState(false);
+    const [secondary, setSecondary] = useState(false);
 
-    console.log(numberOfReports[0])
+    useGetReportQuery({ reportId: `${reports.reportId}`, billingPeriod: `${reports.billingPeriod}` })
+    useGetReportQuery({ reportId: `${reportsMob.reportId}`, billingPeriod: `${reportsMob.billingPeriod}` })
 
-    const report = useGetReportQuery({ reportId: `${reports.reportId}`, billingPeriod: `${reports.billingPeriod}` })
     const { data, error, isLoading } = useGetReportListQuery('')
-
-    // data?.content.map((value: any) => {
-    //     reports.reportId = value.id
-    // })
 
     return (
         <Box sx={{ flexGrow: 1, maxWidth: 1250 }}>
@@ -107,21 +108,42 @@ const ReportList = () => {
                                             ) : data ? (
                                                 <>
                                                     <h3>
-                                                        <>
-                                                            {reports.reportId === '41' ? (
-                                                                <ListItemText
-                                                                    primary={data?.content[0].name + ': ' + data?.content[0].description}
-                                                                    secondary={secondary ? data?.content[0].modifiedDate.toString() : null}
-                                                                />
-                                                            ) : (
-                                                                <ListItemText
-                                                                    primary={data?.content[1].name + ': ' + data?.content[1].description}
-                                                                    secondary={secondary ? 'Secondary text' : null}
-                                                                />
-                                                            )}
-                                                        </>
+                                                        <ListItemText
+                                                            primary={data?.content[0].name + ': ' + data?.content[0].description}
+                                                            secondary={secondary ? data?.content[0].modifiedDate.toString() : null}
+                                                        />
                                                     </h3>
                                                 </>
+                                            ) : null}
+                                        </ListItem>
+                                        <Divider sx={{ bgcolor: "secondary.dark" }} variant="middle" component="li" />
+                                    </Link>
+                                    <Link style={{ textDecoration: 'none' }}
+                                        to={{
+                                            pathname: `${reportsMob.reportId}-${reportsMob.billingPeriod}.json`,
+                                        }}
+                                        state={{ modal: true }}
+                                    >
+                                        <ListItem
+                                            secondaryAction={
+                                                <IconButton edge="end" aria-label="open">
+                                                    <FileOpenIcon />
+                                                </IconButton>}
+                                        >
+                                            <ListItemIcon>
+                                                <ReceiptIcon />
+                                            </ListItemIcon>
+                                            {error ? (
+                                                <>Oh no, there was an error</>
+                                            ) : isLoading ? (
+                                                <>Loading...</>
+                                            ) : data ? (
+                                                <h3>
+                                                    <ListItemText
+                                                        primary={data?.content[1].name + ': ' + data?.content[1].description}
+                                                        secondary={secondary ? 'Secondary text' : null}
+                                                    />
+                                                </h3>
                                             ) : null}
                                         </ListItem>
                                         <Divider sx={{ bgcolor: "secondary.dark" }} variant="middle" component="li" />
